c6728d48ba5ba278e4498f97a03fbbe2609accc5
Kaj Lund <kaj.lund@gmail.com>
2016-06-12 10:52:09 +0300

    Module setup sample

---
+++ 01-module/app/app.module.js
@@ -0,0 +1,10 @@
+(function (app) {
+    'use strict';
+
+    /**
+     * Create a new module using the module setter method.
+     * Second argument is mandatory and can contain modules we depend on
+     */
+    angular
+        .module('app', []);
+}());
